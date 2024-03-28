1d8625c70b4a0ed39c6c968a02fe8600dd477ee1
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-03-28 17:16:40 -0600

    upd

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,16 +1,19 @@
-import { useDispatch } from "react-redux";
-import { changeFilter } from "../../redux/filtersSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
 import css from "./SearchBox.module.css";
 
 const SearchBox = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(selectNameFilter);
 
   return (
     <div>
       <p>Find contacts by name</p>
       <input
         type="text"
+        value={filter}
         onChange={(e) => dispatch(changeFilter(e.target.value))}
+        placeholder="Enter the name"
       />
     </div>
   );
