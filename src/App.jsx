d779572c142178f82dd106cddf4a72efc4625eec
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-03-27 18:22:41 -0600

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
@@ -6,28 +5,6 @@ import SearchBox from "./components/SearchBox/SearchBox";
 
 
 const App = () => {
-  // const [contactList, setContactList] = useState(() => {
-  //   const savedData = localStorage.getItem("data");
-
-  //   return savedData !== null ? JSON.parse(savedData) : contacts;
-  // });
-  // const [filter, setFilter] = useState("");
-
-  // const addContact = (newContact) => {
-  //   setContactList((prevContacts) => {
-  //     return [...prevContacts, newContact];
-  //   });
-  // };
-  // const deleteContact = (id) => {
-  //   setContactList((prevContacts) => {
-  //     return prevContacts.filter((contact) => contact.id !== id);
-  //   });
-  // };
-
-  // const filteredContacts = contactList.filter((filtered) =>
-  //   filtered.name.toLowerCase().includes(filter.toLowerCase())
-  // );
-
   return (
     <div>
       <h1>Phonebook</h1>
